d981a892bf7d276d1be1f6a1ff0eb99e405a08c2
Laurent Paoletti <lp@providenz.fr>
2023-01-18 11:29:07 +0100

    Switch user id to uuid

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
@@ -14,7 +16,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = [
         "username",
     ]
-
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(_("Email address"), unique=True)
     username = models.CharField(_("User name"), max_length=250)
     is_active = models.BooleanField(

9fc80a1054a819b0d9d18dc5f6448a66feaae994
Laurent Paoletti <lp@providenz.fr>
2023-03-09 17:41:15 +0100

    Add password reset workflow

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,4 +1,12 @@
-from django.contrib.auth.views import LoginView, logout_then_login
+from django.contrib.auth.views import (
+    LoginView,
+    PasswordChangeView,
+    PasswordResetCompleteView,
+    PasswordResetConfirmView,
+    PasswordResetDoneView,
+    PasswordResetView,
+    logout_then_login,
+)
 from django.urls import path
 
 from .forms import EmailAuthenticationForm
@@ -14,4 +22,33 @@ urlpatterns = [
         name="login",
     ),
     path("logout/", logout_then_login, name="logout"),
+    path(
+        "password-reset/",
+        PasswordResetView.as_view(
+            template_name="accounts/password_reset_form.html",
+            email_template_name="emails/password_reset/body.html",
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        PasswordResetDoneView.as_view(
+            template_name="accounts/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(
+            template_name="accounts/password_reset_confirm.html",
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        PasswordResetCompleteView.as_view(
+            template_name="accounts/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]

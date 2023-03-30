081a85495bbe3326cb8882efd6e2f26312b084d7
Laurent Paoletti <lp@providenz.fr>
2023-03-30 10:43:36 +0200

    Fix favicon

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,7 +1,9 @@
 """Inspection URL Configuration"""
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 from sheets.views import HomeView
 
@@ -10,6 +12,12 @@ admin.site.index_title = "Administration de l'outil de fiche d'inspection"
 
 
 urlpatterns = [
+    path(
+        "favicon.ico",
+        RedirectView.as_view(
+            permanent=True, url=staticfiles_storage.url("img/favicon.ico")
+        ),
+    ),
     path("grappelli/", include("grappelli.urls")),  # grappelli URLS
     path(f"{settings.ADMIN_SLUG}/", admin.site.urls),
     path("", HomeView.as_view(), name="home"),

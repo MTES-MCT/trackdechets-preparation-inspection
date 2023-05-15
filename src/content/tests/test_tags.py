632ffa71650e73c2d56638ed536fa5998369d134
Laurent Paoletti <lp@providenz.fr>
2023-05-15 09:27:36 +0200

    Comment

--- src/content/tests/test_tags.py
+++ src/content/tests/test_tags.py
@@ -11,6 +11,7 @@ from ..templatetags.survey_tags import survey_callout
 pytestmark = pytest.mark.django_db
 
 
+# test tags
 def test_survey_callout_tag_displays_callout():
     t = Template("{% load survey_tags %}{% survey_callout %}")
     factory = RequestFactory()

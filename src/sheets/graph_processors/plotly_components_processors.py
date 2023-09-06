ff8dbd0f9a096110eae45c3e2b62dd20b49abe88
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2023-09-06 11:16:45 +0200

    Changes the display of 2025 target hbar for ICPE

--- src/sheets/graph_processors/plotly_components_processors.py
+++ src/sheets/graph_processors/plotly_components_processors.py
@@ -1021,25 +1021,26 @@ class ICPEAnnualItemProcessor:
                 font_size=13,
             )
 
-            # Target for 2025
-            fig.add_hline(
-                y=authorized_quantity / 2,
-                line_dash="dot",
-                line_color="black",
-                line_width=2,
-            )
-            fig.add_annotation(
-                xref="x domain",
-                yref="y",
-                x=1,
-                y=authorized_quantity / 2,
-                text=f"Objectif 50% pour 2025 : <b>{format_number_str(authorized_quantity/2,2)}</b> t/an",
-                font_color="black",
-                xanchor="left",
-                showarrow=False,
-                textangle=-90,
-                font_size=12,
-            )
+            if authorized_quantity > 0:
+                # Target for 2025
+                fig.add_hline(
+                    y=authorized_quantity / 2,
+                    line_dash="dot",
+                    line_color="black",
+                    line_width=2,
+                )
+                fig.add_annotation(
+                    xref="x domain",
+                    yref="y",
+                    x=1,
+                    y=authorized_quantity / 2,
+                    text=f"Objectif 50% pour 2025 : <b>{format_number_str(authorized_quantity/2,2)}</b> t/an",
+                    font_color="black",
+                    xanchor="left",
+                    showarrow=False,
+                    textangle=-90,
+                    font_size=12,
+                )
 
             if authorized_quantity > max_y:
                 max_y = authorized_quantity

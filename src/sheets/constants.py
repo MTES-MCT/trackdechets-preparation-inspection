058398f05f19cf6ed645c3d2062a8a9908a737f5
Laurent Paoletti <lp@providenz.fr>
2024-03-27 12:04:27 +0100

    Allow td registry download

--- src/sheets/constants.py
+++ src/sheets/constants.py
@@ -42,3 +42,11 @@ PLOTLY_GRAPHS_TO_RENDER_IN_PDF = [
     "transporter_bordereaux_stats_graph",
     "quantities_transported_stats_graph",
 ]
+
+
+REGISTRY_TYPE_ALL = "ALL"
+REGISTRY_TYPE_INCOMING = "INCOMING"
+REGISTRY_TYPE_OUTGOING = "OUTGOING"
+REGISTRY_TYPE_TRANSPORTED = "TRANSPORTED"
+REGISTRY_FORMAT_CSV = "csv"
+REGISTRY_FORMAT_XLS = "xls"

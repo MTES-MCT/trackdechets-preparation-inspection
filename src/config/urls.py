8c6755178d9af3238aad6fe053322b109e38df87
Laurent Paoletti <lp@providenz.fr>
2024-08-07 10:04:22 +0200

    Update allauth and enhance tests

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,7 +3,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.staticfiles.storage import staticfiles_storage
-from django.http import HttpResponseNotFound
+from django.http import Http404
 from django.urls import include, path
 from django.views.generic.base import RedirectView
 from django_otp.admin import OTPAdminSite
@@ -15,23 +15,28 @@ from sheets.views import PrivateHomeView, PublicHomeView
 admin.site.index_title = "Administration de l'outil de fiche d'inspection"
 admin.site.__class__ = OTPAdminSite
 
+
+def not_found(request):
+    raise Http404()
+
+
 urlpatterns = [
     path(
         "favicon.ico",
         RedirectView.as_view(permanent=True, url=staticfiles_storage.url("img/favicon.ico")),
     ),
-    path(f"{settings.ADMIN_SLUG}/login/", HttpResponseNotFound),  # disable admin logout
+    path(f"{settings.ADMIN_SLUG}/login/", not_found),  # disable admin logout
     path("grappelli/", include("grappelli.urls")),  # grappelli URLS
     path(f"{settings.API_SLUG}/api/v1/", include("api.urls")),
     path(f"{settings.ADMIN_SLUG}/", admin.site.urls),
     path("", PublicHomeView.as_view(), name="public_home"),
     path("home/", PrivateHomeView.as_view(), name="private_home"),
     path("local-accounts/", include("accounts.urls")),
-    path("accounts/signup/", HttpResponseNotFound),
-    path("accounts/login/", HttpResponseNotFound),
-    path("accounts/reauthenticate/", HttpResponseNotFound),
-    path("accounts/password/change/", HttpResponseNotFound),
-    path("accounts/password/reset/", HttpResponseNotFound),
+    # path("accounts/signup/", HttpResponseNotFound, name="allauth_signup"),
+    path("accounts/login/", not_found),
+    path("accounts/logout/", not_found),
+    path("accounts/inactive/", not_found),
+    # path("accounts/3rdparty/signup/", not_found),
     path("accounts/", include("allauth.urls")),
     path("content/", include("content.urls")),
     path("sheets/", include("sheets.urls")),

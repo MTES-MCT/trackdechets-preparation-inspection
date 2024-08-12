a0216e01e13435ef97055b11ee195863fc86d9dd
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2024-08-12 15:14:02 +0200

    Adds more tests

--- src/sheets/tests/test_private_individuals_collections_table_processor.py
+++ src/sheets/tests/test_private_individuals_collections_table_processor.py
@@ -7,28 +7,59 @@ from ..graph_processors.html_components_processors import PrivateIndividualsColl
 @pytest.fixture
 def sample_data():
     # Provide sample data as DataFrames
+    # id 1 and 4 should be processed
     bsda_data = pd.DataFrame(
         {
-            "id": [1, 2, 3],
-            "emitter_is_private_individual": [True, False, True],
-            "recipient_company_siret": ["12345678901234", "12345678901234", "98765432109876"],
-            "worker_company_siret": [None, "12345678901234", None],
-            "emitter_company_name": ["Company A", "Company B", "Company C"],
-            "emitter_company_address": ["Address A", "Address B", "Address C"],
-            "worksite_name": ["Site A", "Site B", None],
-            "worksite_address": ["Address SA", "Address SB", None],
-            "waste_code": ["D123", "D456", "D789"],
-            "waste_name": ["Waste A", "Waste B", "Waste C"],
-            "received_at": [datetime(2024, 8, 10), datetime(2024, 8, 12), datetime(2024, 8, 15)],
+            "id": [1, 2, 3, 4, 5],
+            "emitter_is_private_individual": [True, False, True, True, True],
+            "recipient_company_siret": [
+                "12345678901234",
+                "12345678901234",
+                "98765432109876",
+                "98765432109876",
+                "98765432109871",
+            ],
+            "worker_company_siret": [None, "12345678901234", None, "12345678901234", "12345678901234"],
+            "emitter_company_name": ["Company A", "Company B", "Company C", "Company B", "Company D"],
+            "emitter_company_address": ["Address A", "Address B", "Address C", "Address B", "Address D"],
+            "worksite_name": ["Site A", "Site B", None, "Site B", None],
+            "worksite_address": [
+                "Address SA",
+                "Address SB",
+                None,
+                "Address SB",
+                "Address SD",
+            ],
+            "waste_code": ["17 06 05*", "17 06 05*", "17 03 01*", "17 03 01*", "17 06 05*"],
+            "waste_name": ["Waste A", "Waste A", "Waste B", "Waste B", "Waste A"],
+            "received_at": [
+                datetime(2024, 8, 10),
+                datetime(2024, 8, 12),
+                datetime(2024, 8, 15),
+                datetime(2024, 8, 19),
+                datetime(2023, 8, 19),  # Out of range
+            ],
         }
     )
 
     transport_data = pd.DataFrame(
         {
-            "bs_id": [1, 2, 3],
-            "sent_at": [datetime(2024, 8, 9), datetime(2024, 8, 11), datetime(2024, 8, 14)],
-            "quantity_received": [100, 200, 300],
-            "transporter_company_siret": ["23456789012345", "23456789012345", "34567890123456"],
+            "bs_id": [1, 2, 3, 4, 5],
+            "sent_at": [
+                datetime(2024, 8, 9),
+                datetime(2024, 8, 11),
+                datetime(2024, 8, 14),
+                datetime(2024, 8, 14),
+                datetime(2023, 8, 15),
+            ],
+            "quantity_received": [20, 1.6, 32, 11, 7],
+            "transporter_company_siret": [
+                "23456789012345",
+                "23456789012345",
+                "34567890123456",
+                "34567890123456",
+                "34567890123456",
+            ],
         }
     )
 
@@ -55,7 +86,7 @@ def test_initialization(sample_data, date_interval):
     assert processor.data_date_interval == date_interval
 
 
-def test_empty_data(date_interval):
+def test_empty_data(sample_data, date_interval):
     empty_df = pd.DataFrame()
     processor = PrivateIndividualsCollectionsTableProcessor(
         company_siret="12345678901234",
@@ -63,7 +94,18 @@ def test_empty_data(date_interval):
         bsda_transporters_data_df=None,
         data_date_interval=date_interval,
     )
+    assert processor.build() == []
 
+    bsda_data, transport_data = sample_data
+    bsda_data = bsda_data[
+        ~bsda_data["id"].isin([1, 4])
+    ]  # Get only data that will not be taked into account during the processing
+    processor = PrivateIndividualsCollectionsTableProcessor(
+        company_siret="12345678901234",
+        bsda_data_df=bsda_data,
+        bsda_transporters_data_df=transport_data,
+        data_date_interval=date_interval,
+    )
     assert processor.build() == []
 
 
@@ -79,7 +121,7 @@ def test_data_preprocessing(sample_data, date_interval):
     processor._preprocess_data()
 
     assert processor.preprocessed_data is not None
-    assert len(processor.preprocessed_data) == 1  # Only 1 row should match the company_siret
+    assert len(processor.preprocessed_data) == 2  # Only 1 row should match the company_siret
 
 
 def test_build_output(sample_data, date_interval):
@@ -93,7 +135,11 @@ def test_build_output(sample_data, date_interval):
 
     result = processor.build()
 
-    assert len(result) == 1  # Only 1 row should match both private individual and company_siret
+    assert len(result) == 2  # Only 1 row should match both private individual and company_siret
     assert result[0]["id"] == 1
-    assert result[0]["quantity"] == 100
+    assert result[0]["quantity"] == 20
     assert result[0]["sent_at"] == "09/08/2024 00:00"  # Converted to string format
+
+    assert result[1]["id"] == 4
+    assert result[1]["quantity"] == 11
+    assert result[1]["sent_at"] == "14/08/2024 00:00"  # Converted to string format

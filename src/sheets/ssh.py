86181e85fd187e9822043d4420c366caab6d7a1d
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2025-03-26 09:04:37 +0100

    Updates proc file to launch celery threads  instead of processes

--- src/sheets/ssh.py
+++ src/sheets/ssh.py
@@ -3,11 +3,33 @@ import tempfile
 from contextlib import contextmanager
 
 import sshtunnel
+from django.conf import LazySettings
 
 
 @contextmanager
-def ssh_tunnel(settings):
+def ssh_tunnel(settings: LazySettings):
     temp_key_file = tempfile.NamedTemporaryFile(mode="w", delete=False)
+    """
+    Establishes an SSH tunnel to a remote server and yields the tunnel object.
+    Use it as a context manager to automatically close the SSH connection.
+
+    Parameters
+    ----------
+    settings : Settings
+        A configuration object containing necessary SSH connection details such as host, port, username, key, local bind host, and local bind port.
+
+    Yields
+    ------
+    sshtunnel.SSHTunnelForwarder
+        An SSHTunnelForwarder object representing the active SSH tunnel.
+
+    Notes
+    -----
+    This function creates a temporary file to store the SSH private key securely.
+    It sets the appropriate permissions on the key file before establishing the tunnel.
+    The tunnel is stopped and the key file is deleted when the context manager exits, ensuring cleanup.
+    """
+
     try:
         temp_key_file.write(settings.DWH_SSH_KEY)
         temp_key_file.close()

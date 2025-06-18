c5117278b0f5b0645e3a1fb132cf30b909d7698e
Laurent Paoletti <lp@providenz.fr>
2025-06-18 16:08:05 +0200

    Update src/config/settings/base.py
    Co-authored-by: Luis Ventura <30115537+Lawiss@users.noreply.github.com>

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -219,7 +219,7 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 DEFAULT_FROM_EMAIL = "sender@test.fr"
 MESSAGE_RECIPIENTS = env.list("MESSAGE_RECIPIENTS", [])
 
-OTP_EMAIL_SUBJECT = "Votre code de sécurité pour l'accès à VigieDéchets de Trackdéchetst"
+OTP_EMAIL_SUBJECT = "Votre code de sécurité pour l'accès à Vigiedéchets de Trackdéchets"
 OTP_EMAIL_BODY_TEMPLATE_PATH = "emails/second_factor/second_factor.txt"
 OTP_EMAIL_BODY_HTML_TEMPLATE_PATH = "emails/second_factor/second_factor.html"
 OTP_EMAIL_THROTTLE_DELAY = 300  # s
